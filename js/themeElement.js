ecc555fffef4e53940a9b5d52f71485039d8f171
Marcos Hollanda <marcos.buarque@uol.com.br>
2024-04-08 08:58:47 -0300

    Restructured the object with init() and reset() methods.

--- js/themeElement.js
+++ js/themeElement.js
@@ -4,12 +4,34 @@
 Drupal.themeElement = {
 
   // The object properties.
+  activated: false,
   propertyHook: null,
   suggestions: null,
   filePath: null,
   dataNode: null,
+  beginOutput: false,
+
+  // Store the initial state.
+  initialState: null,
+
+  // Initialize the object.
+  init: function() {
+    // Store the initial state.
+    this.initialState = {
+      activated: this.activated,
+      propertyHook: this.propertyHook,
+      suggestions: this.suggestions,
+      filePath: this.filePath,
+      dataNode: this.dataNode,
+      beginOutput: this.beginOutput
+    };
+  },
 
   // Setter methods.
+  setActivated() {
+    this.activated = true;
+  },
+
   setPropertyHook(input) {
     this.propertyHook = input;
   },
@@ -22,9 +44,16 @@ Drupal.themeElement = {
     this.suggestions = input;
   },
 
-  setDataNode(dataNode) {
-    this.dataNode = dataNode;
-  }
+  setBeginOutput() {
+    this.beginOutput = true;
+  },
 
+  setDataNode(input) {
+    this.dataNode = input;
+  },
 
+  // Reset object to its initial state.
+  reset: function() {
+    Object.assign(this, this.initialState);
+  },
 }
