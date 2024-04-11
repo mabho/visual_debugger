f33bed05f9a0f15d0514fc888a8e84559b175ff2
Marcos Hollanda <marcos.buarque@uol.com.br>
2024-04-11 12:00:26 -0300

    Now using gulp-rename to rename minified Javascript files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp');
 const terser = require('gulp-terser');
+const rename = require('gulp-rename');
 
 gulp.task('minify', function () {
   return gulp.src('js/source/**/*.js')
     .pipe(terser())
+    .pipe(rename({ extname: '.min.js' }))
     .pipe(gulp.dest('js/dist'));
 });
 
