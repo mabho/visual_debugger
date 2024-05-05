b4ebc51294536d2d19b4590c3c83a7f022effbe7
Marcos Buarque de Hollanda <marcos.buarque@uol.com.br>
2024-05-05 12:54:59 -0700

    Create new class VisualDebuggerConstants.
    Carries information on module constants.

---
+++ src/VisualDebuggerConstants.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\visual_debugger;
+
+class VisualDebuggerConstants {
+  const IS_FRONTEND_KEY = 'use_on_frontend';
+  const IS_ADMIN_KEY = 'use_on_admin';
+  const FRONTEND_TAG = 'visual_debugger:config:use_on_frontend';
+  const ADMIN_TAG = 'visual_debugger:config:use_on_admin';
+}
